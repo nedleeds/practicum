54d2bee0f93da4e5f3599b40228528faec953b52
dhl <lllee.dh@gmail.com>
2021-06-29 02:48:56 +0000

    categorize the dataset by disease

--- main.py
+++ main.py
@@ -30,13 +30,54 @@ nmz_mask = normalizing(mask)(fromMinusOne=False, opt="max") # opt="max" : /255. 
 # check    = display(frangi_octa)(numToShow=n, colormap='gray')print(label)
 
 img_label = {}
-dr_dataset = {}
-nc_dataset = {}
 
 for k in nmz_octa.keys():
     if label[int(k)]=="DR" or label[int(k)]=="NORMAL":
         img_label[k]=[label[int(k)]]
         img_label[k].append(nmz_octa[k])
+
+basepath = "/root/Share/data/dataset/disease"
+ogpath = "/root/Share/data/dataset/og"
+drpath = os.path.join(basepath, "DR")
+ncpath = os.path.join(basepath, "NORMAL")
+ampath = os.path.join(basepath, "AMD")
+cnpath = os.path.join(basepath, "CNV")
+cspath = os.path.join(basepath, "CSC")
+rvpath = os.path.join(basepath, "RVO")
+otpath = os.path.join(basepath, "OTHERS")
+
+dr_dataset = {}
+nc_dataset = {}
+am_dataset = {}
+cn_dataset = {}
+cs_dataset = {}
+rv_dataset = {}
+ot_dataset = {}
+
+for i in nmz_octa.keys():
+    k = label[int(i)]
+    v = cv2.imread(os.path.join(ogpath, str(i)+".bmp"), cv2.IMREAD_GRAYSCALE)
+    if k=="DR":
+        cv2.imwrite(os.path.join(drpath, i+"_DR.png"), v)
+    elif k=="NORMAL":
+        nc_dataset[i]=nmz_octa[i]
+        cv2.imwrite(os.path.join(ncpath, i+"_NORMAL.png"), v)
+    elif k=="AMD":
+        am_dataset[i]=nmz_octa[i]
+        cv2.imwrite(os.path.join(ampath, i+"_AMD.png"), v)
+    elif k=="CSC":
+        cs_dataset[i]=nmz_octa[i]
+        cv2.imwrite(os.path.join(cspath, i+"_CSC.png"), v)
+    elif k=="CNV":
+        cn_dataset[i]=nmz_octa[i]
+        cv2.imwrite(os.path.join(cnpath, i+"_CNV.png"), v)
+    elif k=="RVO":
+        rv_dataset[i]=nmz_octa[i]
+        cv2.imwrite(os.path.join(rvpath, i+"_RVO.png"), v)
+    elif k=="OTHERS":
+        ot_dataset[i]=nmz_octa[i]
+        cv2.imwrite(os.path.join(otpath, i+"_OTHERS.png"), v)
+    else: pass
 
 print(img_label)
 
